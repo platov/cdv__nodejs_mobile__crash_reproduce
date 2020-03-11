a94e0baa0612d31b13e33fb548251b1b5d53f9f2
aleksey.platov <aleksey.platov@dev-pro.net>
2020-03-11 16:01:39 +0200

    add cordova dep

--- www/nodejs-project/main.js
+++ www/nodejs-project/main.js
@@ -1,18 +1,15 @@
 const strings = [];
 
-// Create frequenly small strings
 setInterval(() => {
     const str = spawnString(1024 * 1024);
     strings.push(str);
 }, 100);
 
-// Create rarely large strings
 setInterval(() => {
     const str = spawnString(1 * 1024 * 1024);
     strings.push(str);
 }, 500);
 
-// Flush strings arr
 setInterval(() => {
     strings.length = 0;
 }, 10000);
